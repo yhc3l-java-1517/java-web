ab5433e09e0523e8f35b77ef42478f40ed8448aa
Anders Carlsson <anders.carlsson@coredev.se>
2016-02-24 11:27:18 +0100

    Day 1 code and Exercise

---
+++ code/day1/web-day1/src/main/java/se/coredev/web/HelloServlet.java
@@ -0,0 +1,21 @@
+package se.coredev.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public final class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 6058619883426230528L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().print("Hello!");
+	}
+
+}
